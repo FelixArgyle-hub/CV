db0f8bcef01a951fea03654fd2db1ff4393d15ce
FelixArgyle-hub <lenny.sawinski1902@gmail.com>
2024-06-18 15:48:34 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// const sendAge = document.getElementById("sendAge");
+// const displayResultMessage = () => {
+//     const ageUser = Number(document.getElementById("ageUser").value);
+//     const result = document.getElementById("result");
+//     let message;
+
+//     if (ageUser > 0) {
+//         message = `Tu as ${ageUser} an${ageUser > 1 ? 's.' : '.'}`
+//         message += ` Tu es ${ageUser >= 18 ? ' majeur.' : 'mineur.'}`
+//         if(ageUser >= 100) {
+
+//             message += ` Tu es centenaire.`
+//         }
+
+//     } else {
+//         message = "C'est impossible"
+        
+//     }
+//     result.textContent = message
+//     console.log("🚀 ~ displayResultMessage ~ result:", result)
+// }
+// sendAge.addEventListener("click", () => displayResultMessage())
